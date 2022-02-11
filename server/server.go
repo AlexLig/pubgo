9e3495711514a90857bde010bcc0f6a0f005bc33
Alex Ligkas <a.ligkas@ferryhopper.com>
2022-02-11 20:23:12 +0200

    add: bare minimum code to pass the case

--- server/server.go
+++ server/server.go
@@ -16,5 +16,6 @@ type StationServer struct {
 
 func (s *StationServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	stationName := strings.TrimPrefix(r.URL.Path, "/stations/")
+	w.WriteHeader(http.StatusNotFound)
 	fmt.Fprint(w, s.store.GetStationURL(stationName))
 }
